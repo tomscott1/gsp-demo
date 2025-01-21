985f15d93023fdcd25fec2590d98bfd73888c18d
Tom Scott <tomscott85@gmail.com>
2025-01-21 10:18:07 +1000

    remove buttons and frame and make viewer full viewport width with aspect of 4:3

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,6 @@
+import {
+  require_client
+} from "./chunk-B6WEVIQP.js";
+import "./chunk-4X3XUJ2M.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 import Viewer from './components/Viewer';
 
 const App = () => {
+  const [showButtons, setShowButtons] = useState(false);
+
   return (
     <div className="frame">
       <Viewer />
-      <div className="top-right-buttons">
-        <button className="icon-button">
-          <img src="/gsp-demo/icons/mobile.svg" alt="Mobile Icon" className="icon-image" />
-        </button>
-        <button className="icon-button">
-          <img src="/gsp-demo/icons/desktop.svg" alt="Desktop Icon" className="icon-image" />
-        </button>
-        <button className="icon-button">
-          <img src="/gsp-demo/icons/arrows.svg" alt="Arrows Icon" className="icon-image" />
-        </button>
-      </div>
+      {showButtons && (
+        <div className="top-right-buttons">
+          <button className="icon-button">
+            <img src="/gsp-demo/icons/mobile.svg" alt="Mobile Icon" className="icon-image" />
+          </button>
+          <button className="icon-button">
+            <img src="/gsp-demo/icons/desktop.svg" alt="Desktop Icon" className="icon-image" />
+          </button>
+          <button className="icon-button">
+            <img src="/gsp-demo/icons/arrows.svg" alt="Arrows Icon" className="icon-image" />
+          </button>
+        </div>
+      )}
 
-      <div className="bottom-center-buttons">
-        <button className="icon-button">&lt;</button>
-        <button className="icon-button">&gt;</button>
-      </div>
+      {showButtons && (
+        <div className="bottom-center-buttons">
+          <button className="icon-button">&lt;</button>
+          <button className="icon-button">&gt;</button>
+        </div>
+      )}
     </div>
   );
 };
